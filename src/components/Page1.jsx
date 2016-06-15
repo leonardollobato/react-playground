36b3cfa63407bc29f88e16ba445884a4c19c6d5a
Leonardo Lobato <leonardo.lobato@gmail.com>
2016-06-15 02:24:16 -0300

    http fetch installed and functioning

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import HTTP from '../service/httpservice'
 
 class Page1 extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			ingredients: []
+		}
+
+		//this.componentWillMount = this.componentWillMount.bind(this);
+
+	};
+
+	componentWillMount() {
+		HTTP.get('/ingredients').then(function (data) {
+			this.setState({ingredients: data});
+		}.bind(this));
+	};
+
 	render() {
+		var listItems = this.state.ingredients.map(function (item) {
+			return <li key={item.id}>{item.text}</li>;
+		})
 		return (
-			<h1>Page 1</h1>
+			<ul>{listItems}</ul>
 		)
 	}
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,4 @@ import Routes from './Routes.jsx';
 import LeadCapture from './components/LeadCapture.jsx';
 
 ReactDOM.render(
-	<LeadCapture/>, document.querySelector('#main'));
-ReactDOM.render(
-	<LeadCapture/>, document.querySelector('#main1'));
-ReactDOM.render(
-	<LeadCapture/>, document.querySelector('#main2'));
+	<Routes/>, document.querySelector('#main'));
